6f8a8b0d4fbe4a81b0b55c93ffb4ea794e1bca91
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-02-22 23:20:43 -0500

    Add new Repository interface, no implementations yet.

--- src/main/java/cruise/umple/sample/downloader/Repository.java
+++ src/main/java/cruise/umple/sample/downloader/Repository.java
@@ -1,11 +1,28 @@
 package cruise.umple.sample.downloader;
 
+import java.net.URL;
+import java.util.List;
+
 /**
  * Created by kevin on 15-02-22.
  */
 public interface Repository {
 
+    /**
+     * Get the name of the Respository, there are no requirements except not null or empty.
+     * @return Human-friendly name
+     */
     public String getName();
 
-    public String get
+    /**
+     * Get a list of URL instances where the import files may be stored.
+     * @return Non-{@code null}, possibly empty list of Import Files
+     */
+    public List<URL> getImportFiles();
+
+    /**
+     * Checks if the repository is accessible.
+     * @return True if accessible and usable
+     */
+    public boolean isAccessible();
 }
